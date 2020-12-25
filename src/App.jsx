a774f8ab2d2b64d27d8f53ecd8628861844949b1
Sourav <sourav.iit.nstu@gmail.com>
2020-12-25 20:03:57 +0600

    Bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import Home from './mycomponent/Home';
 import About from './mycomponent/About';
 import Services from './mycomponent/Services';
 import Contact from './mycomponent/Contact';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import { Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Navbar from './mycomponent/Navbar';
 
 function App() {
   return (
              <>
+             <Navbar></Navbar>
           <Switch>
               <Route exact path="/" component={Home}/>
               <Route exact path="/about" component={About}/>
